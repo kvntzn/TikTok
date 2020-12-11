dedb4d8bd0eeff9fbc7f6913488afd764ebb99b8
kevin <kevin@PICLKSMLPKEVTUA.IQOR.QOR.COM>
2020-12-11 19:11:58 +0800

    Update post component implemented UI

--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -1,12 +1,16 @@
-import React, { useState } from 'react';
-import {View, Text, TouchableWithoutFeedback} from 'react-native';
+import React, {useState} from 'react';
+import {View, Text, TouchableWithoutFeedback, Image} from 'react-native';
 
 import Video from 'react-native-video';
 import styles from './styles';
 
+import Entypo from 'react-native-vector-icons/Entypo';
+import AntDesign from 'react-native-vector-icons/AntDesign';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import Fontisto from 'react-native-vector-icons/Fontisto';
+
 const Post = () => {
- 
-  const [paused, setPause] =  useState(false);
+  const [paused, setPause] = useState(false);
 
   const onPlayPausePress = () => {
     setPause(!paused);
@@ -17,8 +21,7 @@ const Post = () => {
       <TouchableWithoutFeedback onPress={onPlayPausePress}>
         <Video
           source={{
-            uri:
-              'https://vod-progressive.akamaized.net/exp=1607614862~acl=%2Fvimeo-prod-skyfire-std-us%2F01%2F657%2F17%2F428287241%2F1858680762.mp4~hmac=eff1291d334250865833365e302816c9d400bd2bd9440de56e1b40cc72446aa5/vimeo-prod-skyfire-std-us/01/657/17/428287241/1858680762.mp4?filename=production+ID%3A4625996.mp4',
+            uri: 'https://static.pexels.com/lib/videos/free-videos.mp4',
           }}
           style={styles.video}
           onError={(e) => console.log(e)}
@@ -27,6 +30,55 @@ const Post = () => {
           paused={paused}
         />
       </TouchableWithoutFeedback>
+
+      <View style={styles.uiContainer}>
+        <View style={styles.rightContainer}>
+          <Image
+            style={styles.profilePicture}
+            source={{
+              uri:
+                'https://avatars2.githubusercontent.com/u/29770932?s=460&u=6ea35588386c9b72b388f674045f965056e68abe&v=4',
+            }}
+          />
+
+          <View style={styles.iconContainer}>
+            <AntDesign name={'heart'} size={40} color="white" />
+            <Text style={styles.statsLabel}>123</Text>
+          </View>
+
+          <View style={styles.iconContainer}>
+            <FontAwesome name={'commenting'} size={40} color="white" />
+            <Text style={styles.statsLabel}>123</Text>
+          </View>
+
+          <View style={styles.iconContainer}>
+            <Fontisto name={'share-a'} size={35} color="white" />
+            <Text style={styles.statsLabel}>123</Text>
+          </View>
+        </View>
+
+        <View style={styles.bottomContainer}>
+          <View>
+            <Text style={styles.userName}>@kvntzn</Text>
+            <Text style={styles.description}>
+              crashing waves, summer vacation pls :)
+            </Text>
+
+            <View style={styles.songRow}>
+              <Entypo name={'beamed-note'} size={24} color="white" />
+              <Text style={styles.songName}>RN - Tiktok app</Text>
+            </View>
+          </View>
+          
+          <Image
+            style={styles.songImage}
+            source={{
+              uri:
+                'https://avatars2.githubusercontent.com/u/29770932?s=460&u=6ea35588386c9b72b388f674045f965056e68abe&v=4',
+            }}
+          />
+        </View>
+      </View>
     </View>
   );
 };
